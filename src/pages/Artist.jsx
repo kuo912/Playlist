02f89f953378ba97bd3b46ec22533473bad92519
kuo912 <rkskek7938@naver.com>
2024-11-06 00:21:20 +0900

    추천 아티스트 작업

--- src/pages/Artist.jsx
+++ src/pages/Artist.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
 import Main from '../components/section/Main'
+import { artistText } from '../data/artist'
+import { Link } from 'react-router-dom'
 
 const Artist = () => {
   return (
     <Main
       title="Artist"
       description="아티스트를 추천합니다.">
-      Artist
+      
+      <section id='artistPage'>
+        <h2>😪 추천 아티스트를 소개합니다.</h2>
+        <div className="artist__inner">
+          {artistText.map((artist, key) => (
+            <div className="artist" key={key}>
+              <div className="artist__img play__icon">
+                <Link to={`/channel/${artist.channelId}`}>
+                  <img src={artist.img} alt={artist.name} />
+                </Link>
+              </div>
+              <div className="artist__info">
+              <Link to={`/channel/${artist.channelId}`}>
+                  {artist.name}
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
     </Main>
   )
 }
