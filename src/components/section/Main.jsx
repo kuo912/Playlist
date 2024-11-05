1906a394437997a20c3919c2b6cd4c2570893914
kuo912 <rkskek7938@naver.com>
2024-11-05 23:47:03 +0900

    메인 콘텐츠 작업

--- src/components/section/Main.jsx
+++ src/components/section/Main.jsx
@@ -3,26 +3,29 @@ import { Helmet, HelmetProvider } from 'react-helmet-async'
 
 import Header from './Header'
 import Footer from './Footer'
+import Search from './Search'
 
-const Main = ( props ) => {
-    return (
-        <HelmetProvider>
-            <Helmet 
+const Main = (props) => {
+  return (
+    <HelmetProvider>
+      <Helmet 
                 titleTemplate="%s | KUO's Playlist" 
                 defaultTitle="KUO's Playlist" 
                 defer={false}
             >
                 {props.title && <title>{props.title}</title>}
                 <meta name="description" content={props.description} />
-            </Helmet>
+      </Helmet>
 
-            <Header />
-            <main id="main" role="main">
-                {props.children}
-            </main>
-            <Footer />
-        </HelmetProvider>
-    )
+      <Header />
+      <main id='main' role='main'>
+        <Search />
+        {props.children}
+      </main>
+      <Footer />
+    </HelmetProvider>
+    
+  )
 }
 
 export default Main
