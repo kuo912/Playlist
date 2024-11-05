c1f7fd0e32a1e0bcb1dee925bf5f75cf7e9ee4e5
kuo912 <rkskek7938@naver.com>
2024-11-05 22:32:55 +0900

    섹션 컴퍼넌트 구조화하기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React, { Suspense, lazy } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import Home from './pages/Home'
@@ -14,23 +13,31 @@ import Video from './pages/Video'
 import Search from './pages/Search'
 import Not from './pages/Not'
 
+import Header from './components/section/Header'
+import Main from './components/section/Main'
+import Footer from './components/section/Footer'
+
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
+      <Main>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/today' element={<Today />} />
-          <Route path='/artist' element={<Artist />} />
-          <Route path='/playlist' element={<Playlist />} />
-          <Route path='/latest' element={<Latest />} />
-          <Route path='/musicvideo' element={<Musicvideo />} />
-          <Route path='/pop' element={<Pop />} />
-          <Route path='/kpop' element={<Kpop />} />
-          <Route path='/channel/:channelID' element={<Channel />} />
-          <Route path='/video/:videoID' element={<Video />} />
-          <Route path='/search/:searchID' element={<Search />} />
-          <Route path='/*' element={<Not />}/>
-        </Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/today' element={<Today />} />
+            <Route path='/artist' element={<Artist />} />
+            <Route path='/playlist' element={<Playlist />} />
+            <Route path='/latest' element={<Latest />} />
+            <Route path='/musicvideo' element={<Musicvideo />} />
+            <Route path='/pop' element={<Pop />} />
+            <Route path='/kpop' element={<Kpop />} />
+            <Route path='/channel/:channelID' element={<Channel />} />
+            <Route path='/video/:videoID' element={<Video />} />
+            <Route path='/search/:searchID' element={<Search />} />
+            <Route path='/*' element={<Not />}/>
+          </Routes>
+      </Main>
+      <Footer />
     </BrowserRouter>
   )
 }
