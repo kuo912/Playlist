b52d3f0e57eaa76687b3c57cd9f5d281b31da225
kuo912 <rkskek7938@naver.com>
2024-11-08 00:36:08 +0900

    나머지 페이지 작업

--- src/pages/Artist.jsx
+++ src/pages/Artist.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import Main from '../components/section/Main'
+
 import { artistText } from '../data/artist'
 import { Link } from 'react-router-dom'
 
 const Artist = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 300)
+  },[]);
+
+  const artistPageClass = loading ? 'isLoading':'isLoaded'
+
   return (
     <Main
       title="Artist"
       description="아티스트를 추천합니다.">
       
-      <section id='artistPage'>
+      <section id='artistPage' className={artistPageClass}>
         <h2>😪 추천 아티스트를 소개합니다.</h2>
         <div className="artist__inner">
           {artistText.map((artist, key) => (
