e0092d7babffa78a8cc26c78215c5497ab735c2c
kuo912 <rkskek7938@naver.com>
2025-01-29 21:36:58 +0900

    Update artist.js

--- src/data/artist.js
+++ src/data/artist.js
@@ -1,6 +1,6 @@
 export const artistText = [
     {
-        img: "https://yt3.googleusercontent.com/oZ4JAVZuXZXir560oMZMrrTdUTU8yupSIw58nZ6L4mNrG2rsIuNRPa0vOIf0xE22YILZOw-4Kw=s160-c-k-c0x00ffffff-no-rj",
+        img: "https://yt3.googleusercontent.com/93HLGzoOtjXAR_woouK1XXYSV00bQmRdX-IbdyxprSgyFvcgaJ-sO4g9Cpb6rxunmr-f8dVw7D0=s160-c-k-c0x00ffffff-no-rj",
         name: "LUCY ISLAND",
         channelAddress: "https://www.youtube.com/lucyisland",
         channelId: "UCnXwXrQ8KIBoV8k1T3xGznw"
@@ -12,25 +12,25 @@ export const artistText = [
         channelId: "UCp-pqXsizklX3ZHvLxXyhxw"
     },
     {
-        img: "https://yt3.googleusercontent.com/3XqAERR1uUn8oKMqZaNyt6DfvZJtGG-LcgejMpceDKqu-qQ1FZpQxaDl2_xvJSa7dMtqohlx=s160-c-k-c0x00ffffff-no-rj",
+        img: "https://yt3.googleusercontent.com/gdDXAEju6FQ3_cDoUuyH9CtrMJoUgpxcjl334ltcgvxui_Y0hjc-6IAJ7Fe5bTavyz9w9-Nk8hI=s160-c-k-c0x00ffffff-no-rj",
         name: "あいみょん(아이묭)",
         channelAddress: "https://youtube.com/@aimyong?si=LztyRIcaej11haSj",
         channelId: "UCQVhrypJhw1HxuRV4gX6hoQ"
     },
     {
-        img: "https://yt3.googleusercontent.com/bOwuXswZf4AyKSGsKdQGuT9dR5xt3heaFAJ4sBVLq5OutAvU_A8K3ajgkuPU1jiJuVqstUXX-A=s160-c-k-c0x00ffffff-no-rj",
+        img: "https://yt3.googleusercontent.com/Yv-111Ej7CWFuDvjWmGlir3N3I7YK4U3BMRpU3ZS1nQ0kOGCgw0u_q3lLfQxBuu2NkWSRKfp5g=s160-c-k-c0x00ffffff-no-rj",
         name: "AKMU",
         channelAddress: "https://www.youtube.com/@AKMU",
         channelId: "UCIcXK1CI8URMIqNO_1cehiQ"
     },
     {
-        img: "https://yt3.googleusercontent.com/vU4iKHBMEfPCWmMExvQ239pn8040GWM-0aAvnuyHvZEK_jRkrFo9kdYp5gWjNXS8ck-8JKpLMg=s160-c-k-c0x00ffffff-no-rj",
+        img: "https://yt3.googleusercontent.com/qr2fgVfMdxEn5X_Nx6NklGNzgAckqMmnuX0ARvZ0fG5HAsd7gspmv9B8pSdpmNfjf1MDTeqDEQ=s160-c-k-c0x00ffffff-no-rj",
         name: "터치드 TOUCHED",
         channelAddress: "https://www.youtube.com/@touched_official",
         channelId: "UC1VarYVKLPA6DL8rLclYxBg"
     },
     {
-        img: "https://yt3.googleusercontent.com/YJtgw2DCnDrbM5ejPe1z3sWF_8BNRSFdeqgdQBv2UaXyA2vL7jwq6SUCvbFM__A_MAXyAM0q1jc=s160-c-k-c0x00ffffff-no-rj",
+        img: "https://yt3.googleusercontent.com/PaXg9uoiGVwmySFQz4kEKUDJ9xBq48oBmugWX-B_gB2xhApkWY_aMrhj9xb5wmPrhR0H3af3Kg=s160-c-k-c0x00ffffff-no-rj",
         name: "쏜애플 / THORNAPPLE",
         channelAddress: "https://www.youtube.com/@thornappleofficial",
         channelId: "UCATvEjsg1zfDs7yN_MFUS7A"
@@ -48,7 +48,7 @@ export const artistText = [
         channelId: "UC3dJKwKxhR1Boz56EsI5PYQ"
     },
     {
-        img: "https://yt3.googleusercontent.com/9siqabGI5hSZ_9suwbmbty3LPM1vY3NXtnI968TX2v6lugTVV56F6-gbISJYU_Brc3c4u_ouTQM=s160-c-k-c0x00ffffff-no-rj",
+        img: "https://yt3.googleusercontent.com/82YWRq2rFA-aLefJB71XDauBTyn-VPd9TJaoVTv_xeO3L6pffpAIH--awMCved6G5FP2MzFLyA=s160-c-k-c0x00ffffff-no-rj",
         name: "THISISJUSTHIS",
         channelAddress: "https://www.youtube.com/@THISISJUSTHIS",
         channelId: "UC1YvLoq9XPW5Kg2i9sZGCJg"
@@ -60,7 +60,7 @@ export const artistText = [
         channelId: "UCDJnRpq0DnQhrwi1ruuQa7g"
     },
     {
-        img: "https://yt3.googleusercontent.com/qwk-sEkLbVw1HFVqW4UCsnyTKdwCsNT8mahFIByvlze-rUXbcrd76evbIivNXQAE4E3zvke5=s160-c-k-c0x00ffffff-no-rj",
+        img: "https://yt3.googleusercontent.com/IRxRQGSpQqED3kHlRiUBqFg1DIOAGyulY7CYDpyrExSf-qy94mjUSbWq2n7jx052C90TOKZX5A=s160-c-k-c0x00ffffff-no-rj",
         name: "Ariana Grande",
         channelAddress: "https://www.youtube.com/@ArianaGrande",
         channelId: "UC9CoOnJkIBMdeijd9qYoT_g"
@@ -90,13 +90,13 @@ export const artistText = [
         channelId: "UCymeXjTfl8hMGN7M357oX3g"
     },
     {
-        img: "https://yt3.googleusercontent.com/Kv_mhLwqwpIvW0ORWJmLOBiDWNS4xLppPWqg4JQ85cBZil_s5LNShUDNgNpFCCL0pnisCSERn9Y=s160-c-k-c0x00ffffff-no-rj",
+        img: "https://yt3.googleusercontent.com/_D-kvMpp-b0GUErqWG_nFYnw5TltkRJ--kx-pkdGZygbr_u4eRC4hokmJADCSxRRswPHLN6d=s160-c-k-c0x00ffffff-no-rj",
         name: "Silica Gel 실리카겔",
         channelAddress: "https://www.youtube.com/@SilicaGel",
         channelId: "UCXteMTpHAvyDP05G0QULuZA"
     },
     {
-        img: "https://yt3.googleusercontent.com/IqbbuXds0CP-fpvIDvLIHVW2Z3NCRVvSUYiyL4lDZdLvN3uaknYp_OVawsWAIYyBk0-jPn2o=s160-c-k-c0x00ffffff-no-rj",
+        img: "https://yt3.googleusercontent.com/LE2kMMk1v7wG1DhoSlpDjGvPpqMPRhn4JR7snLaErVx8AJpjs7QBSTr8jb8pQ_XwhQv-wOV1TQ=s160-c-k-c0x00ffffff-no-rj",
         name: "윤도현 Yoon Do Hyun",
         channelAddress: "https://www.youtube.com/@real_YoonDohyun",
         channelId: "UCSmG3xnTJysHBYfBrbDzeAg"
@@ -114,7 +114,7 @@ export const artistText = [
         channelId: "UCycx2ChXHC9bPXoyrPr98cg"
     },
     {
-        img: "https://yt3.googleusercontent.com/9uOEVI04nWG9gIp2dt7wq2Q1ZBnPD-A3RzzarPI2FzK-LJVLKN_DurVOPmCYRQ_4plzAyvlZDUQ=s160-c-k-c0x00ffffff-no-rj",
+        img: "https://yt3.googleusercontent.com/l6shSdLRKL8_oGM0aaA22f52gYrtmbwx7SZ1M5k8TQnu82OdIL43IKYLx2hF0RgZSAUFajRZ=s160-c-k-c0x00ffffff-no-rj",
         name: "다비치 DAVICHI",
         channelAddress: "https://www.youtube.com/@DAVICHIOFFICIAL",
         channelId: "UCwZMmIVr07VLAfNcuqg9LZA"
@@ -126,7 +126,7 @@ export const artistText = [
         channelId: "UCPENYtHg4Xhmm6oX8zaQA7Q"
     },
     {
-        img: "https://yt3.googleusercontent.com/2lOPrJ9wrmOSdDTCvyduiU0viPioC7aid7Ukb426lvV3yTdVzSB6z_34BQAOvTk2SBQ-Xru7zQ=s160-c-k-c0x00ffffff-no-rj",
+        img: "https://yt3.googleusercontent.com/70MNYeqQFUaPH5oHZHCnS_CXtVIyb_KH3Vdfc5ewu5BsV2oGdwGG82VQkmayJGLmsz3wXYIqNw=s160-c-k-c0x00ffffff-no-rj",
         name: "잔나비 JANNABI",
         channelAddress: "https://www.youtube.com/@BandJannabi",
         channelId: "UCMIUx2zV4VvZGyrt4CetTbA"
@@ -144,37 +144,37 @@ export const artistText = [
         channelId: "UClOZoLNwJVdNzkEhzeevBSA"
     },
     {
-        img: "https://yt3.googleusercontent.com/EMHEc8vNqHUC-eBFl_RrLcla7_NH6hEohpi0RrMABe6cOcmCabmnxcqfVFmONWTQg5LtbpNkQg=s160-c-k-c0x00ffffff-no-rj",
+        img: "https://yt3.googleusercontent.com/U3VrCkKjzTpQ3VYv4SCPjNfDHeJV-swGNnhLYhr0nV4lZz_GVUNzK4EB-HFRfKv9S5VNh14uAg=s160-c-k-c0x00ffffff-no-rj",
         name: "BLACKPINK",
         channelAddress: "https://www.youtube.com/@BLACKPINK",
         channelId: "UCOmHUn--16B90oW2L6FRR3A"
     },
     {
-        img: "https://yt3.googleusercontent.com/bbA3XB2VCC1CZPcANyfYdIxoJxNepmwgN8WEojTtWY5FepxztrKXb2nmgXKyJGkSMvsn2NkDug=s160-c-k-c0x00ffffff-no-rj",
+        img: "https://yt3.googleusercontent.com/OMyc4a1u7973d6D60XcKwhPXf_AcKsFY5Rj-H3lnsxkMRqS1tnhPmr4MpjXopEO5AfKHTBFqZA=s160-c-k-c0x00ffffff-no-rj",
         name: "이무진 LEE MU JIN",
         channelAddress: "https://www.youtube.com/@LEEMUJINofficial",
         channelId: "UC10MdW5Ee6ypd9W0NxNbT3g"
     },
     {
-        img: "https://yt3.googleusercontent.com/TQBpA8aJdniKLont5c_n0bIRuOvUupN-pbgtKi3aLqZ-zUUpenkVEyfmxq4eWpiGLx7xrplbtg=s160-c-k-c0x00ffffff-no-rj",
+        img: "https://yt3.googleusercontent.com/UlYl-RufH5HjghT4tZZy1KrBr2S_uIrEVjifVqg509kQC3loP114IEeqhlcqgUfjAHJIqCNTuj0=s160-c-k-c0x00ffffff-no-rj",
         name: "규현 KYUHYUN",
         channelAddress: "https://www.youtube.com/@kyuhyuntv",
         channelId: "UC-QTYOUwcMm-rWCctXiavzA"
     },
     {
-        img: "https://yt3.googleusercontent.com/1DTGWKanZjHTM_N9ieyxm2np0Dtk7gTDOXBKHuGlMEbvgmi5Mv-A47rJApN1dRBC_JEyXazBkQ=s160-c-k-c0x00ffffff-no-rj",
+        img: "https://yt3.googleusercontent.com/l6ARvvtI4GWlZMV0nMCoatBy6NjJIB_PVK4ZTEpImbspN57kMaSqMHskIPXkgxsiISB0inbO9J4=s160-c-k-c0x00ffffff-no-rj",
         name: "ImagineDragons",
         channelAddress: "https://www.youtube.com/@ImagineDragons",
         channelId: "UCT9zcQNlyht7fRlcjmflRSA"
     },
     {
-        img: "https://yt3.googleusercontent.com/-51R7ry0mCpPNNP6HN3KEQ0IsMsAUT30XzjSrJYfLQUynQPMTW9Hjtl-TAoq7upUTxVFTqYf-A=s160-c-k-c0x00ffffff-no-rj",
+        img: "https://yt3.googleusercontent.com/139KQc5HUn17V8jkH47MX2Kq26gx65fI62ECPyyS08L7TS7QIhYk_S3vDmUpb5uvOs8M6uYv=s160-c-k-c0x00ffffff-no-rj",
         name: "Yerin Baek",
         channelAddress: "https://www.youtube.com/@YerinBaekOfficial",
         channelId: "UCYoNPLKd1kXh4v3bvIx1pTA"
     },
     {
-        img: "https://yt3.googleusercontent.com/XCXMXiE_BKZIeUHg72gGgnaSSyGeKtkma_1VFshrTuu0iNVlX2t4Q54oQR20oQiMzAm531Vg9d0=s160-c-k-c0x00ffffff-no-rj",
+        img: "https://yt3.googleusercontent.com/oCZB5vUCcyFBqa18xUjCv9rcCFe6_LVO1mrtgfefjTQfvcBTsQ4rWyphRjGaXsyfnwJIeyLR1w=s160-c-k-c0x00ffffff-no-rj",
         name: "CHRISTOPHER",
         channelAddress: "https://www.youtube.com/@christophermusiccom",
         channelId: "UCWAZJoAnXf9B3xTPbZgGy0g"
@@ -186,43 +186,43 @@ export const artistText = [
         channelId: "UCl-tyxErWS-1ElS8UM8jryQ"
     },
     {
-        img: "https://yt3.googleusercontent.com/CokZrcnMvp5w3RlFWaGAH18633EkblIiAhxFHz3rqNEnBqIoG5RdHcU16mUzdnzqcVWM2EdF=s160-c-k-c0x00ffffff-no-rj",
+        img: "https://yt3.googleusercontent.com/b81YBkLxhGDUoKhYKjtSFqsPkUfGHHwBieg6NMiBW0KwhQEUUqk9SNZ5_s99k6oL0u53_BKkhQ=s160-c-k-c0x00ffffff-no-rj",
         name: "YENA(최예나)",
         channelAddress: "https://www.youtube.com/@YENA_OFFICIAL",
         channelId: "UCkrDEGNM3mqZXHk2MfnOjMw"
     },
     {
-        img: "https://yt3.googleusercontent.com/WW-Vs6WAWiNwvURZWQYea8D-8b-IkcB9AO7_lCd4JN198LsBaj6jTxRN7GscDTgPFw4LDgebdQ=s160-c-k-c0x00ffffff-no-rj",
+        img: "https://yt3.googleusercontent.com/4NWGVojvz2LK2WEjjMkGx7YOS9XUK9830b9Q1TkpaasRMxeXiF0eFmRQhelkMcGx54EbXM3L=s160-c-k-c0x00ffffff-no-rj",
         name: "Heize official",
         channelAddress: "https://www.youtube.com/@Heizeofficial",
         channelId: "UCsXigGjbC_l4ttk-oahTfVg"
     },
     {
-        img: "https://yt3.googleusercontent.com/y9G6c3xhhj0fj1r9QjXVVy6exQiKE3M4HPJpo_mqS13V_9KTzRSZi2xj6jfDP1ctzQruyOLFxWA=s160-c-k-c0x00ffffff-no-rj",
+        img: "https://yt3.googleusercontent.com/ZwshEKb8c3oRXCix0nGGEukLBEgXaOj0E6LMp0QZ-lu7hm9rC3onNLFQAg_ihyDqdWcvqcC7uQ=s160-c-k-c0x00ffffff-no-rj",
         name: "Sia",
         channelAddress: "https://www.youtube.com/@sia",
         channelId: "UCN9HPn2fq-NL8M5_kp4RWZQ"
     },
     {
-        img: "https://yt3.googleusercontent.com/tdh4GB7GFwOd7e4cdull42vhyFYTkpni9bDkTLJn-UFLbuFA1ygJHeibVays0FN2yFNE-0hsdw=s160-c-k-c0x00ffffff-no-rj",
+        img: "https://yt3.googleusercontent.com/dirvtoDAmx-u0UR76-pxfhYL6Wxj2vfL2geUcxDwk62tTWWhGG6QDGc63RG3NdOz38-yBwRHDQ=s160-c-k-c0x00ffffff-no-rj",
         name: "Billie Eilish",
         channelAddress: "https://www.youtube.com/@BillieEilish",
         channelId: "UCiGm_E4ZwYSHV3bcW1pnSeQ"
     },
     {
-        img: "https://yt3.googleusercontent.com/iUO0O5-J4OE-ab3dZOiionUydAqM6nwjSuDgnzPxXgY-ucnL7Gm53nWK8bhgBGkDZzYRPD6J=s160-c-k-c0x00ffffff-no-rj",
+        img: "https://yt3.googleusercontent.com/kPkWipDiZ8Yzx29wNVB5yzUvprmgWkJf7AMoPjE7okzWBVkHnPwERfKjp_gGhwaEqnIJiXA3a4Y=s160-c-k-c0x00ffffff-no-rj",
         name: "노브레인[NOBRAIN] Official",
         channelAddress: "https://www.youtube.com/@officialNOBRAIN",
         channelId: "UCGPvIaYhnCk5Ks9nqIJyGqg"
     },
     {
-        img: "https://yt3.googleusercontent.com/DYnCsjrAdHUB9U_k74gplzERhNlUmaL5pourB2cy9vkX4EkdCEBpjuxoRQN_qIKuEl4q4FRMouQ=s160-c-k-c0x00ffffff-no-rj",
+        img: "https://yt3.googleusercontent.com/SNRyKcCAuJb_0xlfPAnJPqtARXsWJEoqSeR0GBqSpgd1m-FODiBfmQuKlAHjDZVziAxTqHEYEA=s160-c-k-c0x00ffffff-no-rj",
         name: "OfficialLEEHI",
         channelAddress: "https://www.youtube.com/@OfficialLEEHI",
         channelId: "UCT86dWwZWeJWGQCDzbipU-A"
     },
     {
-        img: "https://yt3.googleusercontent.com/4QMqMgtvk6HIPzwuLE3KOtzbdAY6OUeawNZ9Ks9-vKHLTBruoyr7yAcR37pSe7itSu8OMfFISaw=s160-c-k-c0x00ffffff-no-rj",
+        img: "https://yt3.googleusercontent.com/UBzD4kHz4a9Fi-dBrBEYxRNmYdFJE5xHSrGiLFvQuWMDzjNH7LeTrQJwU4GPIr7HDYibSw5z5F4=s160-c-k-c0x00ffffff-no-rj",
         name: "SEVENTEEN",
         channelAddress: "https://www.youtube.com/@pledis17",
         channelId: "UCfkXDY7vwkcJ8ddFGz8KusA"
